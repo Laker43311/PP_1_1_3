bde38a80a682dba04812152ae11a102e7b479cf2
Laker43311 <Laker43311@yandex.ru>
2024-10-09 22:03:30 +0500

    Add project

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
     private UserDao userDao = new UserDaoJDBCImpl();
-
+    @Override
     public void createUsersTable() {
         userDao.createUsersTable();
     }
-
+    @Override
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
@@ -21,14 +21,15 @@ public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
         userDao.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
-
+    @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
-
+    @Override
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
     }
